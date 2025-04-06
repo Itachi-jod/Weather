19ef663ed9e508fa211e73e5ecfea92637ed3b0c
Itachi <itachijodx72@gmail.com>
2025-04-06 13:34:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
+const apiKey = "228dfb40cf77162a98936f1934bc9fdc"; // Replace with your OpenWeatherMap API key
 
 async function getWeather() {
   const city = document.getElementById("cityInput").value.trim();
